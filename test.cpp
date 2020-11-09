d7191ca66e7c350caf126cc11074ea9d3b66b189
Dave <dave@linux.home>
2020-11-09 22:26:54 +0000

    start

---
+++ test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+	int width;
+	width = 5;
+
+	width = 7;
+
+	return 0;
+	
+}
+
+
+A Most Potent Brew - Player Handout
+" D a w n b r e a k s w i t h s t i r r i n g a i r ,
+A s s u n s h i n e s d o w n o n n e w d a y f a i r
+" M i d d a y b l a z e b a k e s e a r t h a n d g r a s s ,
+T h e f a r m e r w a i t s f o r h e a t t o p a s s
+" E v e n i n g c o o l b r i n g s w a t e r , w i n e ,
+D r i n k a n d l a u g h t e r p a s s i n g t i m e
+" N i g h t s e e s s h i n i n g , r o a r i n g f i r e ,
+a s w o o d a n d c o a l s b u r n o n t h e p y r e "
+
+
+" D a w n b r e a k s w i t h s t i r r i n g a i r ,
+A s s u n s h i n e s d o w n o n n e w d a y f a i r
+" M i d d a y b l a z e b a k e s e a r t h a n d g r a s s ,
+T h e f a r m e r w a i t s f o r h e a t t o p a s s
+" E v e n i n g c o o l b r i n g s w a t e r , w i n e ,
+D r i n k a n d l a u g h t e r p a s s i n g t i m e
+" N i g h t s e e s s h i n i n g , r o a r i n g f i r e ,
+a s w o o d a n d c o a l s b u r n o n t h e p y r e
